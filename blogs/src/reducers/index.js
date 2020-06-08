fd3f5a55dd19fd5b9e0d072523662cace35ad722
winstoncwang <congwang@live.co.uk>
2020-06-08 18:01:00 +0100

    T2.6
    add userheader component and add action, reducers to the call

--- blogs/src/reducers/index.js
+++ blogs/src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
 import postsReducer from './postsReducer';
+import usersReducer from './usersReducer';
 
 export default combineReducers({
-	//dummy reducer
-	posts : postsReducer
+	posts : postsReducer,
+	users : usersReducer
 });
